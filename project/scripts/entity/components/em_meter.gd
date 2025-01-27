42a9549c8b70197865ce936bb248b7aea0b32a08
ethobat <edude545@gmail.com>
2025-01-27 21:24:23 +0000

    comment some component scripts

--- project/scripts/entity/components/em_meter.gd
+++ project/scripts/entity/components/em_meter.gd
@@ -3,22 +3,30 @@ class_name EMMeter
 
 var display: MultiDigitDisplay
 
+# This component appears on the EMMeter entity. It reads the entity's dial value (which in this case is provided by a MouseDial component) and uses it to check the EM radiation of whatever entity the player's looking at.
+
 func handle_event(entity: Entity, event: Event) -> Event:
 	match event.event_type:
 		"physicalized":
 			display = event.values[0].get_node("RigidBody3D/EMMeterModel/SubViewport/EMMeterDisplay")
-		"in_hand":
-			var tgt: PhysicalEntity = event.values[0].look_target
+		"in_hand": # "in_hand" - this code gets run every frame while the player is holding this entity.
+			# "noise" is a pseudo-random value that is added to the displayed noise. This is meant to simulate fluctuations in the reading that you would see in real life. Without the noise, the meter would always display a fixed value.
 			var time = Time.get_ticks_msec() / 500.0 
 			var noise = ((sin(time) + sin(time/2) + sin(time/3))/3.0) + 0.01
+			# Get the PhysicalEntity the player is currently looking at.
+			var tgt: PhysicalEntity = event.values[0].look_target
+			# If the player is actually looking at a physical entity:
 			if tgt != null:
+				# Send the em_probe event to the target entity, using this entity's value as the first argument. If the target entity doesn't respond to the event, the return value will be 0.
 				var ret = tgt.entity.fire_event("em_probe", [get_dial_value(entity), 0.0])
+				# Get the return value of this effect, multiplied by 120, and add the random noise.
 				display.num = ret.values[1] * 120 + noise
 			else:
+				# If the player isn't looking at an entity, just output the noise to simulate background radiation.
 				display.num = noise
-		"dephysicalized":
+		"dephysicalized": # When the PhysicalEntity disappears, set display to null. This isn't strictly necessary, it's just for safety, because having a non-physicalized entity with a reference to a node could cause some weird errors.
 			display = null
 	return event
 
-func get_dial_value(entity: Entity):
+func get_dial_value(entity: Entity): # Get the dial value of this entity. In the EM meter's case, the dial value is provided by a MouseDial component.
 	return entity.fire_event("get_dial", [0.0]).values[0]
--- project/scripts/entity/components/mouse_dial.gd
+++ project/scripts/entity/components/mouse_dial.gd
@@ -9,24 +9,42 @@ var dial: Dial = null
 
 func handle_event(_entity: Entity, event: Event) -> Event:
 	match event.event_type:
+		
+		# Respond to the "get_dial" event and return the dial value.
 		"get_dial":
 			event.values[0] = dial_value
-		"physicalized":
+			
+		# When an entity with this component is physicalized, get any child of the PhysicalEntity scene with the Dial script attached to it. This object will be rotated to reflect this entity's dial_value. Look at em_meter_model.tscn for an example of a physical dial.
+		"physicalized": 
 			dial = event.values[0].get_first_child_of_type(Dial)
+			
+		# Every frame while held:
 		"in_hand":
+			
+			 # If the player just started right-clicking, prevent them from looking around by moving the mouse.
 			if Input.is_action_just_pressed("alt_use"):
 				event.values[0].mouse_look_locked = true
+				
+			# While the player is holding the right mouse button, get the horizontal mouse motion and use it to turn the dial.
 			if Input.is_action_pressed("alt_use"):
 				dial_value += event.values[1][0] * dial_sensitivity
-				dial_value = clampf(dial_value, 0.0, 1.0)
-			dial.set_value(dial_value)
+				dial_value = clampf(dial_value, 0.0, 1.0) # Clamp the dial value, so it can't be less than 0 or greater than 1.
+			dial.set_value(dial_value) # The actual turning of the physical dial is handled by its dial script.
+			
+			# Allow the player to look around again once the right mouse button is released.
 			if Input.is_action_just_released("alt_use"):
 				event.values[0].mouse_look_locked = false
+				
+		# Release the mouse look lock once the entity is removed from the player's hand, because they might switch to another item without releasing the right mouse button, and this would cause them to become stuck.
 		"removed_from_hand":
 			event.values[0].mouse_look_locked = false
+			
+		# When the PhysicalEntity disappears, set the physical dial reference to null. This isn't strictly necessary, it's just for safety, because having a non-physicalized entity with a reference to a node could cause some weird errors.
 		"dephysicalized":
 			dial = null
+			
 	return event
 
+# This overrides component's equals function and is used to test equality between components. This MouseDial component is equivalent to another MouseDial component if and only if the dial position and the sensitivity are both the same.
 func equals(comp: Component):
 	return dial_sensitivity == comp.dial_sensitivity and dial_value == comp.dial_value
